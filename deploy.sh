d821ab9dcbc05abb4112dadc73d3da0a31c9b74f
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-04-03 22:56:13 +0200

    Deploy on hd images with spaces in name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-dd if=bootr.img of=$1 bs=1 count=446
-dd if=ide_386l.bin of=$1 bs=512 seek=16
+dd if=bootr.img of="$1" bs=1 count=446 conv=notrunc
+dd if=ide_386l.bin of="$1" bs=512 seek=16 conv=notrunc
 
